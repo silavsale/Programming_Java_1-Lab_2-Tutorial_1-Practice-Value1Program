5f6a05bac04ddc9e896ed31d943780f1f6f49ac8
Rsofku <silavsale@gmail.com>
2017-09-11 13:14:27 -0400

    First commit

---
+++ Value1Program.java
@@ -0,0 +1,10 @@
+public class Value1Program
+{
+	public static void main(String[] args)
+	{
+		int x = 10;
+		char ch = 'G';
+		System.out.println("The value of X variable is 10");
+		System.out.println("The value of variable ch of type char is \t G");
+	}
+}
