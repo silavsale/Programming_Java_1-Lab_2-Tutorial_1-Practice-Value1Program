39f6628d62077f9605b73fec89e52ede46a43018
Rsofku <silavsale@gmail.com>
2017-09-11 16:30:18 -0400

    second edition of file added with void method and new variables

--- Value1Program.java
+++ Value1Program.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Value1Program
 {
-	public static void main(String[] args)
-	{
+	 static Scanner scan = new Scanner(System.in);
+
 		int x = 10;
 		char ch = 'G';
-		System.out.println("The value of X variable is 10");
+		static char val;
+		
+	public static void printValues(){
+		val = scan.next().charAt(0);
+		if (val == 'n' ){
+		System.out.println("The value of X variable is 10");}
+		else{	
 		System.out.println("The value of variable ch of type char is \t G");
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Do you like numbers or letters? ");
+		System.out.print("Please enter your char: ");
+		
+		printValues();
 	}
 }
